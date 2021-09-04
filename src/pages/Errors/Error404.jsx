b69e6d206a51d1abc65305b0d2aa0ab0e53f9707
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2021-09-03 19:09:23 -0500

    ✨ add: function the back button and polishing the style of the same component a bit

--- src/pages/Errors/Error404.jsx
+++ src/pages/Errors/Error404.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './Error404.scss'
 import { ReactComponent as ErrorSvg404 } from "assets/svgs/404_two.svg";
 import { NavPages } from 'components/NavPages/NavPages';
-export const Error404 = () => {
+export const Error404 = (props) => {
+
     return (
         <>
-            <NavPages titleHeaderPage="Volver" />
+            <NavPages titleHeaderPage="Volver" history={props.history} />
 
             <div className="container-404">
                 <div className="container-404__content">
