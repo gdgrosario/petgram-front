144118a1d6545b2fe55141226a3315862cab6d20
g1alexander <alexlds26@gmail.com>
2021-10-04 21:42:00 -0500

    fix: relocations of logic routes

---
+++ src/routes/LoadRoutes/Components/LoadSubroutesHome.jsx
@@ -0,0 +1,6 @@
+import { LoadSubRoutes } from "routes/LoadRoutes/Components/LoadSubRroutes";
+import React from "react";
+
+export const LoadSubroutesHome = ({ routes }) => {
+  return <LoadSubRoutes routes={routes} />;
+};
--- src/routes/routes.config.js
+++ src/routes/routes.config.js
@@ -3,50 +3,49 @@ import { SignUp } from "pages/Auth/SignUp";
 import { Error404 } from "pages/Errors/Error404";
 import { Followers } from "pages/Followers/Followers";
 import { Home } from "pages/Home/Home";
-import { LoadSubroutesHome } from "pages/Home/LoadSubroutesHome";
+import { LoadSubroutesHome } from "routes/LoadRoutes/Components/LoadSubroutesHome";
 import { Team } from "pages/Team/Team";
 import { UserProfile } from "pages/UserProfile/UserProfile";
 
 export const routes = [
-    {
-        path: '/',
-        exact: false,
-        component: LoadSubroutesHome,
-        routes: [
-            {
-                path: '/',
-                exact: true,
-                component: Home,
-            },
-            {
-                path: '/Login',
-                exact: true,
-                component: Login,
-            },
-            {
-                path: '/SignUp',
-                exact: true,
-                component: SignUp,
-            },
-            {
-                path: '/Team',
-                exact: true,
-                component: Team,
-            },
-            {
-                
-                path: '/Followers',
-                component: Followers,
-                exact: true
-            },
-            {
-                path: '/:UserName',
-                component: UserProfile,
-                exact: true
-            },
-            {
-                component: Error404
-            }
-        ]
-    },
-]
+  {
+    path: "/",
+    exact: false,
+    component: LoadSubroutesHome,
+    routes: [
+      {
+        path: "/",
+        exact: true,
+        component: Home,
+      },
+      {
+        path: "/login",
+        exact: true,
+        component: Login,
+      },
+      {
+        path: "/sign-up",
+        exact: true,
+        component: SignUp,
+      },
+      {
+        path: "/team",
+        exact: true,
+        component: Team,
+      },
+      {
+        path: "/followers",
+        component: Followers,
+        exact: true,
+      },
+      {
+        path: "/:username",
+        component: UserProfile,
+        exact: true,
+      },
+      {
+        component: Error404,
+      },
+    ],
+  },
+];
