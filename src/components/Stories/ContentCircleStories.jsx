e6e710d45f12a09f92caf9f94d2628eca2965077
OLiver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2021-10-22 06:26:21 -0500

    🐛 fix: use values context auth

--- src/components/Stories/ContentCircleStories.jsx
+++ src/components/Stories/ContentCircleStories.jsx
@@ -1,13 +1,17 @@
+import { AuthContext } from "context/ContextProvider"
+import { useContext } from "react"
 import { CircleStories } from "./CircleStories"
 
 export const ContentCircleStories = () => {
+    const { user: UserState } = useContext(AuthContext)
+    const { user, isLoading } = UserState
     return (
         <section className="content-circle-stories">
-            <CircleStories />
-            <CircleStories />
-            <CircleStories />
-
-
+            {user && !isLoading && <>
+                <CircleStories />
+                <CircleStories />
+                <CircleStories />
+            </>}
         </section>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,9 @@
 import { Hero } from "./components/Hero";
-import { useContext } from "react";
-import { AuthContext } from "context/ContextProvider";
 import { FooterActionButtons } from "components/FooterActionButtons";
 import { HeaderHome } from "components/HeaderHome";
 import { ContentCircleStories } from "components/Stories/ContentCircleStories";
 
 export function Home() {
-  const { user: UserState } = useContext(AuthContext)
-  const { user, isLoading } = UserState
-
   return (
     <>
       <HeaderHome/>
