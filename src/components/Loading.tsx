52248993a4dc67d77218fffdd04c25622e6e233e
Daniel Aguilar <dyangeltk17@gmail.com>
2022-06-09 15:14:05 -0500

    add: new loader component has been created

---
+++ src/components/Loading.tsx
@@ -0,0 +1,16 @@
+export function Loading () {
+  return (
+    <div className="content-loading">
+      <div className="content-loading__bar content-loading__bar-purple"></div>
+      <div className="content-loading__bar content-loading__bar-pink"></div>
+      <div className="content-loading__bar content-loading__bar-red"></div>
+      <div className="content-loading__bar content-loading__bar-orange"></div>
+      <div className="content-loading__bar content-loading__bar-yellow"></div>
+      <div className="content-loading__bar content-loading__bar-yellow"></div>
+      <div className="content-loading__bar content-loading__bar-orange"></div>
+      <div className="content-loading__bar content-loading__bar-red"></div>
+      <div className="content-loading__bar content-loading__bar-pink"></div>
+      <div className="content-loading__bar content-loading__bar-purple"></div>
+    </div>
+  )
+}
