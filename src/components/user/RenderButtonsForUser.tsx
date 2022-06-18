fbc226a20ac2b895177b90b62d79f5498b6ee7eb
g1alexander <alexlds26@gmail.com>
2022-06-18 16:31:35 -0500

    feat: part 1 update actions profile

--- src/components/user/RenderButtonsForUser.tsx
+++ src/components/user/RenderButtonsForUser.tsx
@@ -1,13 +1,60 @@
-import { Button } from '@components/user/Button'
+import { Button } from '@components/user/Button';
+import { AuthContext } from '@context/ContextProvider';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { User } from '../../models/User';
+import { setCookie } from '../../helpers/getCookies';
+interface IProfileUser {
+  userData: User;
+}
+export const RenderButtonsForUser = ({ userData }: IProfileUser) => {
+  const { user } = useContext(AuthContext);
 
-export const RenderButtonsForUser = ({ user, isLoading }) => (
+  const router = useRouter();
+
+  const handleAction = (action: string) => {
+    if (!user) {
+      router.push('/Auth/SignIn');
+    }
+  };
+
+  const handleCloseSession = () => {
+    setCookie('user_token', '', -1);
+    router.push('/');
+  };
+
+  return (
     <div className="info-user-box__box-buttons">
-      {user && !isLoading && <Button textButtonn="Editar perfil" />}
-      {!user && !isLoading && (
+      {user.email === userData.email && (
         <>
-          <Button textButtonn="Seguir" />
-          <Button textButtonn="Enviar mensaje" />
+          <Link href={`/profile/edit`}>
+            <a>
+              <Button textButtonn="Editar perfil" />
+            </a>
+          </Link>
+
+          <Button
+            textButtonn="Cerrar Sesión"
+            onClick={() => handleCloseSession()}
+          />
+        </>
+      )}
+      {(!user || user.email !== userData.email) && (
+        <>
+          <Button
+            textButtonn="Seguir"
+            onClick={() => handleAction('follower')}
+          />
+
+          {user.email !== userData.email && (
+            <Button
+              textButtonn="Dejar de seguir"
+              onClick={() => handleAction('unfollower')}
+            />
+          )}
         </>
       )}
     </div>
-)
+  );
+};
