70a93a476b19f20c248b19cd85cdd3e8ebe39f8b
g1alexander <alexlds26@gmail.com>
2022-06-18 16:30:38 -0500

    chore: delete button menu

--- src/components/NavPages.tsx
+++ src/components/NavPages.tsx
@@ -1,10 +1,9 @@
 /* svgs */
-import { useRouter } from 'next/router'
-import BackBtn from '@public/assets/svgs/icons/arrow.svg'
-import MenuBtn from '@public/assets/svgs/icons/bar.svg'
+import { useRouter } from 'next/router';
+import BackBtn from '@public/assets/svgs/icons/arrow.svg';
 
-export function NavPages ({ titleHeaderPage, history }) {
-  const router = useRouter()
+export function NavPages({ titleHeaderPage, history }) {
+  const router = useRouter();
 
   return (
     <header className="header-nav-pages">
@@ -23,13 +22,7 @@ export function NavPages ({ titleHeaderPage, history }) {
             <h1 className="header-nav-pages__titlePage">{titleHeaderPage}</h1>
           </li>
         )}
-
-        <li className="header-nav-pages__item">
-          <button className="header-nav-pages__btn">
-            <MenuBtn className="header-nav-pages__svg header-nav-pages__svg--pointer" />
-          </button>
-        </li>
       </ul>
     </header>
-  )
+  );
 }
