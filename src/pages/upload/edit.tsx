619a141e519577f4618f20d8a413ad42f9aff45a
g1alexander <alexlds26@gmail.com>
2022-07-09 14:28:23 -0500

    feat: upload image complete

--- src/pages/upload/edit.tsx
+++ src/pages/upload/edit.tsx
@@ -1,12 +1,20 @@
 import { HeadInfo } from '@components/HeadInfo';
 import { UploadContext } from '@context/ContextUpload';
 import Close from '@public/assets/svgs/icons/close.svg';
+import { Upload } from '@services/Upload';
 import { useRouter } from 'next/router';
 import { useContext, useEffect } from 'react';
 
 export default function edit() {
   const router = useRouter();
-  const { image } = useContext(UploadContext);
+  const {
+    image,
+    imageBlob,
+    description,
+    setDescription,
+    setUpload,
+    setimageBlob,
+  } = useContext(UploadContext);
 
   useEffect(() => {
     if (!image) {
@@ -14,22 +22,40 @@ export default function edit() {
     }
   }, [image]);
 
+  const handleChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleHome = () => {
+    setUpload(null);
+    setDescription('');
+    setimageBlob(null);
+    router.push('/');
+  };
+
+  const handleSubmit = async () => {
+    await Upload({ image: imageBlob, description });
+  };
+
   return (
     <>
       <HeadInfo title="Subir foto" />
 
       <header className="header">
-        <Close />
+        <Close onClick={handleHome} />
 
         <h2>Nuevo post</h2>
 
-        <button>siguiente</button>
+        <button onClick={handleSubmit}>siguiente</button>
       </header>
 
-      <form>
-        <textarea>holakskaiksj</textarea>
-        <img src={image} alt="" width={50} height={50} />
-      </form>
+      <section className="container__upload">
+        <img src={image} alt="" width={70} height={70} />
+        <textarea
+          defaultValue={description}
+          onChange={(event) => handleChange(event)}
+        ></textarea>
+      </section>
     </>
   );
 }
