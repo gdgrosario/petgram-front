db9dca455bebf0eb4470237043bb43370e354dfc
g1alexander <alexlds26@gmail.com>
2022-07-09 12:21:40 -0500

    feat: part1 upload image

--- src/components/HeaderHome.tsx
+++ src/components/HeaderHome.tsx
@@ -1,22 +1,59 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-import Logo from '@public/assets/svgs/Logo.svg'
-import AddHistory from '@public/assets/svgs/icons/add_history.svg'
-import Message from '@public/assets/svgs/icons/message.svg'
-import Favorite from '@public/assets/svgs/icons/favorite.svg'
+import Logo from '@public/assets/svgs/Logo.svg';
+import AddHistory from '@public/assets/svgs/icons/add_history.svg';
+import Message from '@public/assets/svgs/icons/message.svg';
+import Favorite from '@public/assets/svgs/icons/favorite.svg';
+import { useContext, useRef } from 'react';
+import { UploadContext } from '../context/ContextUpload';
+import { useRouter } from 'next/router';
+import { getCompressor } from '@helpers/getCompressor';
 
 export const HeaderHome = () => {
+  const router = useRouter();
+
+  const imageSelector = useRef(null);
+  const { setUpload, setimageBlob } = useContext(UploadContext);
+
+  const onSelectImage = () => {
+    imageSelector.current.click();
+  };
+
+  const onSelectedImage = async (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+    if (!file) return;
+    const fr = new FileReader();
+    fr.onload = () => setUpload(fr.result as string);
+    fr.readAsDataURL(file);
+
+    const result = await getCompressor(file);
+
+    setimageBlob(result);
+
+    router.push('/upload');
+  };
+
   return (
     <header className="header-home container-global">
       <Logo className="header-logo" />
       <nav className="header-home__nav">
         <ul className="header-home__list">
           <li className="header-home__item header-home__item--box">
-            <Link href="/">
-              <a className="header-home__link">
-                <AddHistory className="header-home__icon" />
-              </a>
-            </Link>
+            <button
+              onClick={() => onSelectImage()}
+              className="header-home__link"
+            >
+              <AddHistory className="header-home__icon" />
+            </button>
+
+            <input
+              type="file"
+              capture="user"
+              accept="image/*"
+              onChange={() => onSelectedImage(event)}
+              ref={imageSelector}
+            />
           </li>
           <li className="header-home__item">
             <Link href="/">
@@ -35,5 +72,5 @@ export const HeaderHome = () => {
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
--- src/pages/upload/edit.tsx
+++ src/pages/upload/edit.tsx
@@ -1,7 +1,19 @@
 import { HeadInfo } from '@components/HeadInfo';
+import { UploadContext } from '@context/ContextUpload';
 import Close from '@public/assets/svgs/icons/close.svg';
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
 
 export default function edit() {
+  const router = useRouter();
+  const { image } = useContext(UploadContext);
+
+  useEffect(() => {
+    if (!image) {
+      router.push('/');
+    }
+  }, [image]);
+
   return (
     <>
       <HeadInfo title="Subir foto" />
@@ -16,12 +28,7 @@ export default function edit() {
 
       <form>
         <textarea>holakskaiksj</textarea>
-        <img
-          src="https://via.placeholder.com/300x"
-          alt=""
-          width={50}
-          height={50}
-        />
+        <img src={image} alt="" width={50} height={50} />
       </form>
     </>
   );
