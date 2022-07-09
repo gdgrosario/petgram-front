1a6c66ab595a8deb2a3b95edf4758484a58a4cac
g1alexander <alexlds26@gmail.com>
2022-07-09 14:28:49 -0500

    chore: change types

--- src/context/ContextUpload.tsx
+++ src/context/ContextUpload.tsx
@@ -2,14 +2,18 @@ import { createContext, Dispatch, SetStateAction, useState } from 'react';
 
 interface IUploadContext {
   image: string | null;
-  imageBlob: Blob | null;
+  imageBlob: File | null;
+  description: string;
+  setDescription: Dispatch<SetStateAction<string | null>>;
   setUpload: Dispatch<SetStateAction<string | null>>;
-  setimageBlob: Dispatch<SetStateAction<Blob | null>>;
+  setimageBlob: Dispatch<SetStateAction<File | null>>;
 }
 
 export const UploadContext = createContext<IUploadContext>({
   image: null,
   imageBlob: null,
+  description: '',
+  setDescription: () => {},
   setUpload: () => {},
   setimageBlob: () => {},
 });
@@ -19,11 +23,15 @@ export const UploadProvider = ({ children }) => {
 
   const [imageBlob, setimageBlob] = useState(null);
 
+  const [description, setDescription] = useState('');
+
   return (
     <UploadContext.Provider
       value={{
         image,
         imageBlob,
+        description,
+        setDescription,
         setUpload,
         setimageBlob,
       }}
