d6a0543e3beecab92ecb546c119ac035c4a70200
g1alexander <alexlds26@gmail.com>
2022-09-11 10:26:08 -0500

    styles: home complete

--- src/components/HeadInfo.tsx
+++ src/components/HeadInfo.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 interface HeadInfoProps {
   title: string;
@@ -7,12 +7,13 @@ interface HeadInfoProps {
 
 export const HeadInfo = ({
   title,
-  description = 'Bienvenido a petgram',
+  description = "Bienvenido a petgram",
 }: HeadInfoProps) => {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="theme-color" content="#f6f6f6"></meta>
       <link rel="manifest" href="/manifest.json" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
